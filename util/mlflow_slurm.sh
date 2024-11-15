3f33b4ee270e6bd2b47c77a5a8e8ee12eeedac76
Max Spadafore <maxspad@gl-login3.arc-ts.umich.edu>
2024-11-15 10:55:20 -0500

    mlflow job works well

--- util/mlflow_slurm.sh
+++ util/mlflow_slurm.sh
@@ -1,7 +1,25 @@
+#!/bin/bash
 
 echo "Starting mlflow..."
-conda activate nlp-qual-um
-jobid=$(sbatch --parsable mlflow.sbatch.sh)
+#conda activate nlp-qual-um
+jobid=$(sbatch --parsable util/mlflow.sbatch.sh)
 
 echo "Jobid is $jobid"
-squeue -j $jobid -o "%N"
+echo "Waiting for node to be populated..."
+while true
+do
+	echo "Waiting for node to be acquired..."
+	node=$(squeue -j $jobid -o "%N" | tail -1)
+	if [ ! -z "${node}" ]
+	then
+		echo "Node is $node"
+		break
+	fi
+	sleep 3
+done
+
+echo "SSH port forward to node $node on port 5000..."
+ssh -f -N -L 5000:localhost:5000 $node
+echo "Done."
+
+#squeue -j $jobid -o "%N"
